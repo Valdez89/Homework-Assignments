5d0f15c2a3e8b3f5b92bcbc4a5fb8e51840e2aea
Stewart Valdez <108932805+Valdez89@users.noreply.github.com>
2023-03-23 17:03:53 -0700

    Updated the second comment for alert prompt.

--- Block07hwk/main.js
+++ Block07hwk/main.js
@@ -5,7 +5,7 @@ let x = 10*1;
 let y = 400/10;
 let z = 60-40+19;
 
-/* Here will be the results via the alert prompts followed by the const variables to display the combinations */
+/* Here will be the results via the alert prompts followed by the declared variables being displayed to show the combination. */
 
 alert(
     "You have received this message because you have been chosen to open an important vault. Here is the secret combination: " +
